82b7a104b2be2986a8b03d4c2c04fe3d237a1c1e
entrptaher@gmail.com <entrptaher@gmail.com>
2018-02-03 02:13:24 +0600

    Basic Persistance

--- src/store.js
+++ src/store.js
@@ -1,17 +1,21 @@
 import { observable, action, autorun } from "mobx";
+import { create, persist } from "mobx-persist";
 
 function randomString(len = 16, charSet) {
-  charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var randomString = '';
+  charSet =
+    charSet || "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var randomString = "";
   for (var i = 0; i < len; i++) {
-      var randomPoz = Math.floor(Math.random() * charSet.length);
-      randomString += charSet.substring(randomPoz,randomPoz+1);
+    var randomPoz = Math.floor(Math.random() * charSet.length);
+    randomString += charSet.substring(randomPoz, randomPoz + 1);
   }
   return randomString;
 }
 
 class Store {
-  @observable todos = [{ id: randomString(), text: "Hello from Mobx" }];
+  @persist("list")
+  @observable
+  todos = [{ id: randomString(), text: "Hello from Mobx" }];
 
   // various store related actions
   @action
@@ -41,11 +45,18 @@ class Store {
 // create a store and export it
 // You can access the store from any component
 const store = new Store();
-
-// this will run everytime there is a change in store
-autorun(() => {
-  console.log(store.todos.length);
-});
-
 export default store;
-window.store = store;
+
+// OPTIONALS
+
+// Save in localstorage, it'll be loaded automatically
+const hydrate = create({
+  storage: localStorage,
+  jsonify: true
+});
+const rehydrate = hydrate("HelloWorld", store).rehydrate;
+
+// update localstorage on interval (on all tabs)
+setInterval(() => {
+  rehydrate();
+}, 500);
